b55ddd187ec092c1252162c345c0684aa38c7364
victory0901 <jfloyd@fullsail.edu>
2012-07-15 20:31:10 -0400

    Trying to get alerts working

---
+++ js/alert.js
@@ -0,0 +1,34 @@
+    // Wait for PhoneGap to load
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    // PhoneGap is ready
+    function onDeviceReady() {
+ 
+    }
+	
+	$("#note").on('click', function() {
+    	showAlert();
+    	makeBeep();
+    });
+    
+    // Dismiss Alert
+    function alertDismissed() {
+    
+    }
+
+    // Show a custom alert
+    function showAlert() {
+        navigator.notification.alert(
+            'Alerts are working!',  // message
+            alertDismissed,         // callback
+            'Game Over',            // title
+            'Done'                  // buttonName
+        );
+    }
+    
+    // Make beep sound
+    function makeBeep() {
+    navigator.notification.beep(1);
+    }
+
+
