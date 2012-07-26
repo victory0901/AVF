1c6e5ad95593e3bcc5c4f658aef2fbc38b951d65
victory0901 <jfloyd@fullsail.edu>
2012-07-26 00:57:49 -0400

    Remove JQM from CRUD app form

--- js/main.js
+++ js/main.js
@@ -1,95 +1,34 @@
-$("#home").bind('pageinit', function(){
-	console.log("I am ready");
-	$.couch.db("asdproject").view("app/gifts", {
-		success: function(data){
-			console.log(data);
-			$.each(data.rows, function(index, gift){
-				var group = gift.value.group[1]; 
-				var fname = gift.value.fname[1];
-				var lname = gift.value.lname[1];
-				var giftx = gift.value.gift[1];	
-				var quantity = gift.value.quantity[1];
-				var purchase = gift.value.purchase[1];
-				var buydate = gift.value.buydate[1];
-				var notes = gift.value.notes[1];  
-				$(''+
-                        '<ul>' +
-                        '<li>'+ 'Group: ' + group + '</li>' +
-                        '<li>'+ 'Name: ' + fname + " " + lname +'</li>'+
-                        '<li>'+ 'Gift: ' + giftx +'</li>'+	
-                        '<li>'+ 'Quantity: ' + quantity +'</li>'+
-                        '<li>'+ 'Purchase: ' + purchase +'</li>'+
-                        '<li>'+ 'Buy Date: ' + buydate +'</li>'+
-                        '<li>'+ 'Notes: ' + notes +'</li>'+ 
-                        '</ul>'	+ '<br />'
-                     ).appendTo('#giftlist');
-			});
-			$("#giftlist").listview("refresh");
-		}
-	});
-	
-	/*	
-	$.ajax({
-		"url": "_view/gifts",
-		"dataType": "json",
-		"success": function(data){
-			$.each(data.rows, function(index, gift){
-				var group = gift.value.group[1]; 
-				var fname = gift.value.fname[1];
-				var lname = gift.value.lname[1];
-				var giftx = gift.value.gift[1];	
-				var quantity = gift.value.quantity[1];
-				var purchase = gift.value.purchase[1];
-				var buydate = gift.value.buydate[1];
-				var notes = gift.value.notes[1];  
-				$(''+
-                        '<ul>' +
-                        '<li>'+ 'Group: ' + group + '</li>' +
-                        '<li>'+ 'Name: ' + fname + " " + lname +'</li>'+
-                        '<li>'+ 'Gift: ' + giftx +'</li>'+	
-                        '<li>'+ 'Quantity: ' + quantity +'</li>'+
-                        '<li>'+ 'Purchase: ' + purchase +'</li>'+
-                        '<li>'+ 'Buy Date: ' + buydate +'</li>'+
-                        '<li>'+ 'Notes: ' + notes +'</li>'+ 
-                        '</ul>'	+ '<br />'
-                     ).appendTo('#giftlist');
-				console.log(data);
-				
-			});
-			$("#giftlist").listview("refresh");
-		}
-	});
- */
 
-    var aiform = $('#additemform'),
-        aierrorslink = $('#aierrorslink')
-    ;
+//Wait for DOM to be ready
+window.addEventListener("DOMContentLoaded", function(){
 
-    aiform.validate({
-        invalidHandler: function(form, validator){
-            aierrorslink.click();
-            var html = '';
-            for(var key in validator.submitted){
-                var label = $('label[for^="'+ key +'"]').not('[generated]');
-                var legend = label.closest('fieldset').find('.ui-controlgroup-label');
-                var fieldName = legend.length ? legend.text() : label.text();
-                html += '<li>'+ fieldName +'</li>';
-            };
-            $("#additemerrors ul").html(html);
-        },
-        submitHandler: function(){
-            storeData();
-        }
-    });
-
-
+	//getElementById Function
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+	//Create select field element and populate with options
+	function makeCats(){
+		var formTag = document.getElementsByTagName("form"), //formTag is an array of all forms
+			selectLi= $("select"),
+			makeSelect = document.createElement("select");
+			makeSelect.setAttribute("id", "groups");
+		for(var i=0, j=contactGroups.length; i<j; i++){
+			var makeOption = document.createElement("option");
+			var optText = contactGroups[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}	
+		selectLi.appendChild(makeSelect);
+	}
 	
 	//Find the value of selected radio button
 	function getSelectedRadio(){
-		var radios = $("form").local;
+		var radios = document.forms[0].local;
 		for(var i=0; i<radios.length; i++){
 			if (radios[i].checked){
-				purchaseValue = radios[i].val();
+				purchaseValue = radios[i].value;
 			}
 		}
 	}
@@ -97,91 +36,79 @@ $("#home").bind('pageinit', function(){
 	function toggleControls(n){
 		switch(n){
 			case "on":
-				$("contactForm").css("display","none");
-				$("clear").css("display","inline");
-				$("displayLink").css("display","none");
-				$("addNew").css("display","inline");
+				$("contactForm").style.display = "none";
+				$("clear").style.display = "inline";
+				$("displayLink").style.display = "none";
+				$("addNew").style.display = "inline";
 				break;
 			case "off":
-				$("contactForm").css("display","block");
-				$("clear").css("display","inline");
-				$("displayLink").css("display","inline");
-				$("addNew").css("display","none");
-				$("items").css("display","none");
+				$("contactForm").style.display = "block";
+				$("clear").style.display = "inline";
+				$("displayLink").style.display = "inline";
+				$("addNew").style.display = "none";
+				$("items").style.display = "none";
 				break;
 			default:
 				return false;
 		}
 	}
 	
-	function storeData(key, rev){
+	function storeData(key){
 	//If there is no key, this means this is a brand new item and we need a new key.
 	if(!key){
 		var id  		= Math.floor(Math.random()*10000001);
 	}else {
 		//Set the id to the existing key we're editing so that it will save over the data.
-		//The key is the same key that's been passed along from the editSubmit event handler
+		//The key is the same key that's been passed along from the editSubmit envent handler
 		//To the validate function, and then passed here, into the storeData function.
 		id = key;
 	}
 	getSelectedRadio();
-	var item			= {};
-		item._id		= key;
-		item._rev		= rev;
-		item.group		= ["Group:", $("groups").val()];
-		item.fname		= ["First Name:", $("fname").val()];
-		item.lname		= ["Last Name:", $("lname").val()];
-		item.gift		= ["Gift:", $("gift").val()];
-		item.quantity	= ["Quantity:", $("quantity").val()];
+	var item		= {};
+		item.group		= ["Group:", $("groups").value];
+		item.fname		= ["First Name:", $("fname").value];
+		item.lname		= ["Last Name:", $("lname").value];
+		item.gift		= ["Gift:", $("gift").value];
+		item.quantity	= ["Quantity:", $("quantity").value];
 		item.purchase	= ["Where to Buy:", purchaseValue]; 
-		item.buydate	= ["Buy Date:", $("buydate").val()];
-		item.notes		= ["Notes:", $("notes").val()];
-		
+		item.buydate	= ["Buy Date:", $("buydate").value];
+		item.notes		= ["Notes:", $("notes").value];
 		//Save data into Local Storage: Use Stringify to convert our object
-		$.couch.db('asdproject').saveDoc(item, {
-        	success: function(data) {
-                alert("Gift saved.");
-        	}
-        });
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("Contact Saved!");
 		
 	}
 	
 	function getData(){
-	
 		toggleControls("on");
 		if(localStorage.length === 0){
 			alert("There is no data in local storage, so default data was added.");
 			autoFillData();
 		}
-		
 		//Write Data from Local Storage to the browser.
-		var makeDiv = $("div");
-		makeDiv.attr("id", "items");
-		
-		var makeList = $("ul");
-		makeDiv.append(makeList);
-//		document.body.append(makeDiv);
-		$("items").css("display","block");
-		
+		var makeDiv = document.createElement("div");
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement("ul");
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$("items").style.display = "block";
 		for(var i=0, len=localStorage.length; i<len; i++){
-			var makeLi = $("li");
-			var linksLi = $("li");
-			makeList.append(makeLi);
+			var makeLi = document.createElement("li");
+			var linksLi = document.createElement("li");
+			makeList.appendChild(makeLi);
 			var key = localStorage.key(i);
 			var value = localStorage.getItem(key);
-			
 			//Convert a string from local storage value back to an object by using JSON.parse
 			var obj = JSON.parse(value);
-			var makeSubList = $("ul");
-			makeLi.append(makeSubList);
+			var makeSubList = document.createElement("ul");
+			makeLi.appendChild(makeSubList);
 			getImage(obj.group[1], makeSubList);
-			
 			for(var n in obj){
-				var makeSubLi = $("li");
-				makeSubList.append(makeSubLi);
+				var makeSubLi = document.createElement("li");
+				makeSubList.appendChild(makeSubLi);
 				var optSubText = obj[n][0]+" "+obj[n][1];
-				makeSubLi.append(optSubText);
-				makeSubList.append(linksLi);
+				makeSubLi.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
 			}
 			makeItemLinks(localStorage.key(i), linksLi);  //Create our edit and delete buttons/links for each item in Local Storage
 		}
@@ -189,50 +116,43 @@ $("#home").bind('pageinit', function(){
 	
 	//Get Image for the right category
 	function getImage (catName, makeSubList){
-		var imageLi = $("li");
-		makeSubList.append(imageLi);
-		var newImg = $("img");
-		var setSrc= newImg.attr("src", "images/"+ catName + ".png");
-		imageLi.append(newImg);
+		var imageLi = document.createElement("li");
+		makeSubList.appendChild(imageLi);
+		var newImg = document.createElement("img");
+		var setSrc= newImg.setAttribute("src", "images/"+ catName + ".png");
+		imageLi.appendChild(newImg);
 	}
 	
 	//Auto Populate Local Storage
 	function autoFillData() {
 		//The actual JSON JOBKJECT data required for this to work is coming from our json.js file, which is laoaded from our HTML page.
 		//Store the JSON OBJECT in local storage.
-/*		for(var n in json) {
+		for(var n in json) {
 			var id = Math.floor(Math.random()*10000001);
 			localStorage.setItem(id, JSON.stringify(json[n]));
-		} */
-	} 
+		}
+	}
 	
 	//Make Item Links
 	//Creat the edit and delete links for each stored item when displayed
 	function makeItemLinks(key, linksLi){
-		
 		//add edit single item link
-		var editLink = $("a");
-		 editLink.prop("href","#");
+		var editLink = document.createElement("a");
+		 editLink.href = "#";
 		editLink.key = key;
-		
 		var editText = "Edit Gift ";
-		$(editLink).bind("click", function(){
-			editItem(key);
-		});
-		editLink.html = editText; 
-		linksLi.append(editLink);
+		editLink.addEventListener("click", editItem);
+		editLink.innerHTML = editText; 
+		linksLi.appendChild(editLink);
 		
 		//add delete single item link
-		var deleteLink = $("a");
-		deleteLink.prop("href", "#");
+		var deleteLink = document.createElement("a");
+		deleteLink.href = "#";
 		deleteLink.key = key;
-		
 		var deleteText = "Delete Gift";
-		$(deleteLink).bind("click", function(){
-			deleteItem(key);
-		});
-		deleteLink.html = deleteText;
-		linksLi.append(deleteLink);
+		deleteLink.addEventListener("click", deleteItem);
+		deleteLink.innerHTML = deleteText;
+		linksLi.appendChild(deleteLink);
 	}
 	
 	function editItem(){
@@ -244,30 +164,30 @@ $("#home").bind('pageinit', function(){
 		toggleControls("off");
 		
 		//Populate the form fields with current localStorage values.
-		$("groups").val() = item.group[1];
-		$("fname").val() = item.fname[1];
-		$("lname").val() = item.lname[1];
-		$("gift").val() = item.gift[1];
-		$("quantity").val() = item.quantity[1];
-		var radios = $("form").local;
+		$("groups").value = item.group[1];
+		$("fname").value = item.fname[1];
+		$("lname").value = item.lname[1];
+		$("gift").value = item.gift[1];
+		$("quantity").value = item.quantity[1];
+		var radios = document.forms[0].local;
 		for(var i=0; i<radios.length; i++){
-			if(radios[i].val() == "online" && item.purchase[1] == "online"){
-				radios[i].attr("checked", "checked");
-			}else if(radios[i].val() == "store" && item.purchase[1] == "store"){
-				radios[i].attr("checked", "checked");
+			if(radios[i].value == "online" && item.purchase[1] == "online"){
+				radios[i].setAttribute("checked", "checked");
+			}else if(radios[i].value == "store" && item.purchase[1] == "store"){
+				radios[i].setAttribute("checked", "checked");
 			}
 		}
-		$("buydate").val() = item.buydate[1];
-		$("notes").val() = item.notes[1];
+		$("buydate").value = item.buydate[1];
+		$("notes").value = item.notes[1];
 		
-		save.unbind("click", storeData);
-
-		$("submit").val() = "Edit Contact";
+		//Remove the initial listener from the input 'save contact' button.
+		save.removeEventListener("click", storeData);
+		//Change Submit button Value to say edit button
+		$("submit").value = "Edit Contact";
 		var editSubmit = $("submit");
-		$(editSubmit).on("click", function(){
-			storeData(key);
-			return false;		
-		});	
+		//Save the key value established in this function as aproperty of the editSubmit event
+		//so we can use that value when we save the date we edited
+		editSubmit.addEventListener("click", validate);
 		editSubmit.key = this.key;
 	}
 	
@@ -293,14 +213,78 @@ $("#home").bind('pageinit', function(){
 		}
 	}
 	
+	function validate(e){
+		//Define the elements we want to check
+		var getGroup = $("groups");
+		var getFname = $("fname");
+		var getLname = $("lname");
+		var getGift = $("gift");
+		
+		//Reset Error Messages
+		errMsg.innerHTML = "";
+		getGroup.style.border = "1px solid black";
+		getFname.style.border = "1px solid black";
+		getLname.style.border = "1px solid black";
+		getGift.style.border = "1px solid black";
+		
+		//Get error messages
+		var messageAry = [];
+		//Group validation
+		if(getGroup.value === "--Choose A Group--"){
+			var groupError = "Please choose a group.";
+			getGroup.style.border = "1px solid red";
+			messageAry.push(groupError);
+		}
+		
+		//First Name Validation
+		if(getFname.value === ""){
+			var fNameError = "Please enter a first name.";
+			getFname.style.border = "1px solid red";
+			messageAry.push(fNameError);
+		}
+		
+		//Last Name Validation
+		if(getLname.value === ""){
+			var lNameError = "Please enter a last name.";
+			getLname.style.border = "1px solid red";
+			messageAry.push(lNameError);
+		}
+		
+		//Gift Validation
+		if(getGift.value === ""){
+			var giftError = "Please a gift.";
+			getGift.style.border = "1px solid red";
+			messageAry.push(giftError);
+		}
+		
+		//If there were errors display them on the screen
+		if(messageAry.length >= 1){
+			for(var i=0, j=messageAry.length; i < j; i++){
+				var txt = document.createElement("li");
+				txt.innerHTML = messageAry[i];
+				errMsg.appendChild(txt);
+			}
+			e.preventDefault();
+			return false;
+		}else {
+			//If all is ok save data! Send the key value (which came from the editData function)
+			//Remember this key value was passed through the editSubmit event listener as a property.
+			storeData(this.key);
+		}
+	}
+	
+	 //Variable Defaults
+	var contactGroups = ["--Choose A Group--", "Family", "Non-Family"],
+		purchaseVaule,
+		errMsg = $("errors");
+	;
+	makeCats();
 
 	//Set Link & Submit Click Events
-	var displayLink =  $("#displayLink");
-	displayLink.bind("click", getData);
-	var clearLink = $("#clear");
-	clearLink.bind("click", clearLocal); 
-	var save = $("#submit");
-	$("#submit").bind('click',function () {
-		storeData();
-	});
+	var displayLink =  $("displayLink");
+	displayLink.addEventListener("click", getData);
+	var clearLink = $("clear");
+	clearLink.addEventListener("click", clearLocal); 
+	var save = $("submit");
+	save.addEventListener("click", validate);
 });
